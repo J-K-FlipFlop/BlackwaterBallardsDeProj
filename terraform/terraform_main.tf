5f168762697adee8c3ded20d8561547c9b0f8462
BenTurner7 <Ben.HT77@yahoo.co.uk>
2024-05-14 10:38:07 +0100

    finished the code that creates the s3 bucket and relevant profile access issues

--- terraform/terraform_main.tf
+++ terraform/terraform_main.tf
@@ -1,3 +1,19 @@
-resource "aws_s3_bucket" "tf_ingestion_zone" {
-    bucket = "blackwater-ingestion-zone"
+terraform {
+  required_providers {
+    aws={
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "blackwater-state"
+    key = "state-bucket/terraform.tfstate"
+    region = "eu-west-2"
+  }
 }
+
+provider "aws" {
+  region = "eu-west-2"
+  profile = "blackwater"
+}
+
---
+++ terraform/terraform_s3.tf
@@ -0,0 +1,6 @@
+
+
+resource "aws_s3_bucket" "tf_ingestion_zone" {
+    bucket = "blackwater-ingestion-zone"
+}
+
