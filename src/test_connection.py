d64d11ff98b21d7bdd431548592d95391d568700
BenTurner7 <Ben.HT77@yahoo.co.uk>
2024-05-14 12:26:24 +0100

    review of test connection, happy that it is connected and pulling the correct data, very minor changes to test file (prettyprint) and adding pg8000 to requirements file

--- src/test_connection.py
+++ src/test_connection.py
@@ -1,10 +1,11 @@
 from src.connection import connect_to_db
+from pprint import pprint as pp
 
 def test_query():
     conn = connect_to_db()
     result = conn.run("""SELECT * FROM design limit 5;""")
     conn.close()
-    print(result)
+    pp(result)
     return result
 
 test_query()
