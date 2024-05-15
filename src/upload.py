6ee269dd480a17cd1e1178a534b0c5fe6cf8dea4
Richard Wilding <richard_wilding@hotmail.com>
2024-05-15 15:23:04 +0100

    complete and test write_csv_to_s3 function

--- src/upload.py
+++ src/upload.py
@@ -25,10 +25,15 @@ def write_to_s3(client, data, bucket, key):
     
 
 def write_csv_to_s3(session, data, bucket, key):
-    response =  wr.s3.to_csv(
-        df=pd.DataFrame(data),
-        path=f's3://{bucket}/{key}',
-        boto3_session=session
-        )
-    print(response)
-    return {"status": "success", "message": "written to bucket"}
+    try:
+        response =  wr.s3.to_csv(
+            df=pd.DataFrame(data),
+            path=f's3://{bucket}/{key}',
+            boto3_session=session,
+            index=False
+            )
+        return {"status": "success", "message": "written to bucket"}
+    except ClientError as c:
+        logger.info(f"Boto3 ClientError: {str(c)}")
+        return {"status": "failed", "message": c.response["Error"]["Message"]}
+    
