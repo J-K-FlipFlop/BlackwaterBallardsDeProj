e604c1dfe8fefedd623781141c985bdffbbbc678
Richard Wilding <richard_wilding@hotmail.com>
2024-05-15 14:17:08 +0100

    create write_csv_to_s3 function using awswrangler

--- src/upload.py
+++ src/upload.py
@@ -2,6 +2,8 @@ from src.handler import connect_to_db_table, create_csv_data
 import boto3
 import logging
 from botocore.exceptions import ClientError
+import awswrangler as wr
+import pandas as pd
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -20,3 +22,13 @@ def write_to_s3(client, data, bucket, key):
     except ClientError as c:
         logger.info(f"Boto3 ClientError: {str(c)}")
         return {"status": "failed", "message": c.response["Error"]["Message"]}
+    
+
+def write_csv_to_s3(session, data, bucket, key):
+    response =  wr.s3.to_csv(
+        df=pd.DataFrame(data),
+        path=f's3://{bucket}/{key}',
+        boto3_session=session
+        )
+    print(response)
+    return {"status": "success", "message": "written to bucket"}
--- test/test_upload.py
+++ test/test_upload.py
@@ -1,9 +1,9 @@
 import pytest
 import boto3
 import os
-import moto
 from moto import mock_aws
-from src.upload import lambda_handler, write_to_s3
+from src.upload import lambda_handler, write_to_s3, write_csv_to_s3
+import datetime
 
 
 @pytest.fixture(scope="function")
@@ -72,3 +72,48 @@ class TestWriteToS3:
             response["Body"].read().decode("UTF-8")
             == "hello, collumn2, the_roman_empire, homer_simpson"
         )
+
+
+class TestWriteCsvToS3:
+    def test_csv_file_is_written_to_bucket(self, s3_client):
+        session = boto3.session.Session(
+        aws_access_key_id="test", aws_secret_access_key="test"
+    )
+        data = [
+            {
+                "staff_id": 1,
+                "first_name": "Jeremie",
+                "last_name": "Franey",
+                "department_id": 2,
+                "email_address": "jeremie.franey@terrifictotes.com",
+                "created_at": datetime.datetime(
+                    2022, 11, 3, 14, 20, 51, 563000
+                ),
+                "last_updated": datetime.datetime(
+                    2022, 11, 3, 14, 20, 51, 563000
+                ),
+            },
+            {
+                "staff_id": 2,
+                "first_name": "Deron",
+                "last_name": "Beier",
+                "department_id": 6,
+                "email_address": "deron.beier@terrifictotes.com",
+                "created_at": datetime.datetime(
+                    2022, 11, 3, 14, 20, 51, 563000
+                ),
+                "last_updated": datetime.datetime(
+                    2022, 11, 3, 14, 20, 51, 563000
+                ),
+            },
+        ]
+        bucket = "bucket-for-my-emotions"
+        key = "folder/file.csv"
+
+        s3_client.create_bucket(
+            Bucket=bucket,
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
+        result = write_csv_to_s3(session, data, bucket, key)
+        assert result["message"] == "written to bucket"
+
