54a385959ff2eb906d3df0fcbb8a2387d3687892
15ML <mikey.5881@gmail.com>
2024-05-17 14:25:49 +0100

    added read s3 permission

--- terraform/terraform_s3.tf
+++ terraform/terraform_s3.tf
@@ -3,14 +3,14 @@ resource "aws_s3_bucket" "tf_ingestion_zone" {
     bucket = "blackwater-ingestion-zone"
 }
 
-#bucket for safe storage of lambda code (if doesn't exist will create):
-resource "aws_s3_bucket" "extract_lambda_storage" {
-  bucket = "blackwater-code-lambda-storage"
-}
+# #bucket for safe storage of lambda code (if doesn't exist will create):
+# resource "aws_s3_bucket" "extract_lambda_storage" {
+#   bucket = "blackwater-code-lambda-storage"
+# }
 
-#resource to store lambda code within 'extract lambda storage' bucket:
-resource "aws_s3_object" "extract_lambda_code" {
-  bucket = aws_s3_bucket.extract_lambda_storage.bucket
-  key = "lambda/extract_lambda.zip"
-  source = "${path.module}/../function.zip"
-}
+# #resource to store lambda code within 'extract lambda storage' bucket:
+# resource "aws_s3_object" "extract_lambda_code" {
+#   bucket = aws_s3_bucket.extract_lambda_storage.bucket
+#   key = "lambda/extract_lambda.zip"
+#   source = "${path.module}/../function.zip"
+# }
