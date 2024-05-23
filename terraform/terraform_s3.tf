e532261c368faf7b2ebd39d0dae322a1b563f5fc
BenTurner7 <Ben.HT77@yahoo.co.uk>
2024-05-23 11:23:20 +0100

    first attempts at writing the ingestion bucket notification trigger

--- terraform/terraform_s3.tf
+++ terraform/terraform_s3.tf
@@ -3,6 +3,16 @@ resource "aws_s3_bucket" "tf_ingestion_zone" {
   bucket = "blackwater-ingestion-zone"
 }
 
+resource "aws_s3_bucket_notification" "ingestion_notification" {
+  bucket = "blackwater-ingestion-zone"
+  lambda_function {
+    lambda_function_arn = "transform-lambda-arn-to-be-added"
+    events = ["s3:ObjectCreated:"]
+    filter_prefix = "last_ran_at"
+    filter_suffix = ".csv"
+  }
+}
+
 #bucket for storage of extracted totesys data (if doesn't exist will create):
 resource "aws_s3_bucket" "tf_processed_zone" {
   bucket = "blackwater-processed-zone"
